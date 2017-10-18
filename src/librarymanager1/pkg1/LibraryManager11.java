e838b3c17215c2e0710195c0b98009baf18df741
Alex LUFUNGULA <Alex LUFUNGULA@DESKTOP-VKV9O3K>
2017-10-18 09:34:48 +0300

    Quelqles arangement des codes pour une meilleure visibilitE

--- src/librarymanager1/pkg1/LibraryManager11.java
+++ src/librarymanager1/pkg1/LibraryManager11.java
@@ -5,9 +5,7 @@
  */
 package librarymanager1.pkg1;
 
-import java.util.ArrayList;
-import java.util.List;
-import librarymanager1.Entities.Book;
+
 import librarymanager1.IHM.LibManag1;
 
 /**
@@ -20,20 +18,9 @@ public class LibraryManager11 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //
         LibManag1 formManager11= new LibManag1();
         formManager11.setVisible(true);
-//        List<Book> listBook = new ArrayList<>();
-        //
-        ConnectionSQLite connSQLite = new ConnectionSQLite();
-        String requett = "SELECT * FROM book";
-        //
-        Object[][] tab= connSQLite.SelectEXE(requett);
-        for (int i = 0; i < tab.length; i++) {
-            for (int j = 0; j < tab[0].length; j++) {
-                System.out.println(tab[i][j]);
-            }
-        }
     }
 
 }
